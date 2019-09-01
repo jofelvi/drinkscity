8379d0683630cbd60af9b40c9af4eb072d778e33
David Noreña <david@packen.co>
2019-09-01 14:08:55 -0500

    rut change limit3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,21 +36,6 @@ function checkRut(rut) {
 	cuerpo = valor.slice(0,-1);
 	dv = valor.slice(-1).toUpperCase();
 	rut.value = cuerpo + '-'+ dv
-
-	suma = 0;
-	multiplo = 2;
-    
-	for(i=1;i<=cuerpo.length;i++) {
-		index = multiplo * valor.charAt(cuerpo.length - i);
-		suma = suma + index;
-		if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }  
-	}
-
-	dvEsperado = 11 - (suma % 11);
-	dv = (dv == 'K')?10:dv;
-	dv = (dv == 0)?11:dv;
-	//if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }   
-	rut.setCustomValidity('');
 }
 
 // Format price
