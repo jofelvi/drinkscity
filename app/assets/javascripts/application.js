9cd56190c496e520820f7ba3a1aecdbc960809f0
David Noreña <david@packen.co>
2019-09-01 14:26:45 -0500

    rut change limit41

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,12 +31,47 @@ function validate(evt) {
 
 // Rut validator chile
 function checkRut(rut) {
-	var valor = rut.value.replace('.','');
-	valor = valor.replace('-','');
-	cuerpo = valor.slice(0,-1);
-	dv = valor.slice(-1).toUpperCase();
-	rut.value = cuerpo + '-'+ dv
+  rut = String(rut);
+  var valor = rut.replace(".", "").replace(".", "");
+  valor = valor.replace("-", "");
+  cuerpo = valor.slice(0, -1);
+  dv = valor.slice(-1).toUpperCase();
+  rut = cuerpo + "-" + dv;
+  if (cuerpo.length < 7) {
+    return false;
+  }
+  suma = 0;
+  multiplo = 2;
+  for (i = 1; i <= cuerpo.length; i++) {
+    index = multiplo * valor.charAt(cuerpo.length - i);
+    suma = suma + index;
+    if (multiplo < 7) {
+      multiplo = multiplo + 1;
+    } else {
+      multiplo = 2;
+    }
+  }
+  dvEsperado = 11 - suma % 11;
+  dv = dv == "K" ? 10 : dv;
+  dv = dv == 0 ? 11 : dv;
+  if (dvEsperado != dv) {
+    return false;
+  }
+  return true;
 }
+
+$.validator.addMethod("validRut", function(value, element) {
+      return checkRut(value);
+    }, "Debes ingresar un rut válido");
+
+$('#data-form').validate({
+  rules: {
+    rut: {
+      required: true,
+      validRut: true
+    }
+  }
+});
 
 // Format price
 function format(input){
