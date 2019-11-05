de8b451815d6e960c44eff34ee7595d2ca766730
pdrowr <pdrowr@gmail.com>
2019-11-05 18:47:48 -0400

    fixed current order bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def current_order
     if !session[:order_id].nil?
-      Order.find(session[:order_id])
+      Order.find_by_id(session[:order_id])
     else
       Order.new
     end
