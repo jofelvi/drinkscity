32c8010fc2b6376f187d3ef086d57dc700cadc99
pdrowr <pdrowr@gmail.com>
2019-11-05 18:50:48 -0400

    fixed current order bug

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,18 +3,18 @@ class Admin::HomeController < ApplicationController
 
   def index
   	if current_user.role = "admin"
-  		@orders = Order.all
-  		@stores = Store.all
-  		@products = Product.all
-  		@users = User.all
-  		@events = Event.all
-  		@products_quantity = Order.joins(:order_items).where(order_status_id: 4).sum("order_items.quantity" )
+  		@orders = Order&.all
+  		@stores = Store&.all
+  		@products = Product&.all
+  		@users = User&.all
+  		@events = Event&.all
+  		@products_quantity = Order&.joins(:order_items)&.where(order_status_id: 4)&.sum("order_items.quantity" )
 
-      @array = @stores.map { |e| [e.name, e.products.count]}
-      @array2 = @users.where(role: "store_admin").map { |e| [e.email, e.orders.count]}
+      @array = @stores&.map { |e| [e&.name, e&.products&.count]}
+      @array2 = @users&.where(role: "store_admin")&.map { |e| [e&.email, e&.orders&.count]}
 
 	 	end
   end
 
-  
+
 end
